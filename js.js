5176d1ca051eb01585f354b245791835c22e5948
jeangondorek <jeangondorek73@gmail.com>
2021-02-23 17:37:11 -0300

    Mais
    Atualização de botão para ajudar na hora de baixar/imprimir arquivo

--- js.js
+++ js.js
@@ -2,12 +2,15 @@ var clicado = false;
 function hide(el){
     var display = document.getElementById(el).style.display;
     document.getElementById(el).style.display = 'none';
+    document.getElementById('baixar').style.bottom = "-2%";
 }
 
 function hide1(el){
     var display = document.getElementById(el).style.display;
     document.getElementById('titleacademy').style.display = "none";
     document.getElementById(el).style.display = 'none';
+    document.getElementById('baixar').style.bottom = "-2%";
+    
 }
 
 function funcao1(){
@@ -15,4 +18,11 @@ function funcao1(){
         alert('Para remover um campo, de duplo clique nele!');
         clicado = true;
     }
+}
+
+function baixa(){
+    document.getElementById('baixar').style.display = "none";
+    document.getElementById('corpo').style.height = "auto";
+    window.print();
+
 }
